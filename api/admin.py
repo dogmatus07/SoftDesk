b974ab48a5a389faf40d02c14cd81d3657b7a743
Steve <steve.raharison@gmail.com>
2024-09-21 10:42:48 +0300

    Endpoints success

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
+"""
+Import the models from the api app and register them with the admin site.
+"""
 from django.contrib import admin
+from .models import (
+    Project,
+    Contributor,
+    Issue,
+    Comment,
+)
 
-# Register your models here.
+
+# register models
+admin.site.register(Project)
+admin.site.register(Contributor)
+admin.site.register(Issue)
+admin.site.register(Comment)
--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,6 @@
+"""
+Import the path function from django.urls and the views from the api app.
+"""
 from django.urls import path
 from .views import (
     ProjectListCreateView,
@@ -14,20 +17,23 @@ from .views import (
 urlpatterns = [
     # project urls
     path("projects/", ProjectListCreateView.as_view(), name="project-list-create"),
-    path("projects/<uuid:pk>/", ProjectDetailView.as_view(), name="project-detail"),
+    path("projects/<int:pk>/", ProjectDetailView.as_view(), name="project-detail"),
 
     # contributor urls
-    path("contributors/", ContributorListView.as_view(), name="contributor-list-create"),
     path(
-        "contributors/<uuid:pk>/", 
-        ContributorDetailView.as_view(), 
+        "contributors/",
+        ContributorListView.as_view(),
+        name="contributor-list-create"),
+    path(
+        "contributors/<int:pk>/",
+        ContributorDetailView.as_view(),
         name="contributor-detail"),
-    
+
     # issue urls
     path("issues/", IssueListCreateView.as_view(), name="issue-list-create"),
-    path("issues/<uuid:pk>/", IssueDetailView.as_view(), name="issue-detail"),
+    path("issues/<int:pk>/", IssueDetailView.as_view(), name="issue-detail"),
 
     # comment urls
     path("comments/", CommentListCreateView.as_view(), name="comment-list-create"),
-    path("comments/<uuid:pk>/", CommentDetailView.as_view(), name="comment-detail"),
+    path("comments/<int:pk>/", CommentDetailView.as_view(), name="comment-detail"),
 ]
