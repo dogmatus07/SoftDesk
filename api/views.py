afcbf31e9cf2f058376f24df0c09e53b7ead980c
Steve <steve.raharison@gmail.com>
2024-10-04 18:30:54 +0300

    Permissions added, Views updated

--- api/views.py
+++ api/views.py
@@ -5,22 +5,44 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from api.models import Project, Contributor, Issue, Comment
+from .permissions import IsActiveUser
 from .serializers import (
     ProjectSerializer,
     ContributorSerializer,
     IssueSerializer,
     CommentSerializer,
+    UserSerializer
 )
 
 
+# User Delete View
+
+class UserDeleteView(generics.DestroyAPIView):
+    """
+    Delete a user instance RGPD compliant
+    """
+    permission_classes = [permissions.IsAuthenticated, IsActiveUser]
+    serializer_class = UserSerializer
+
+
+    def get_object(self):
+        return self.request.user
+
+    def perform_destroy(self, instance):
+        instance.delete()
+
+
 # Project views
+@method_decorator(cache_page(60*15), name='dispatch')
 class ProjectListCreateView(generics.ListCreateAPIView):
     """
     List all projects or create a new project
     """
     serializer_class = ProjectSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsActiveUser]
 
 
     def get_queryset(self):
@@ -47,7 +69,7 @@ class ProjectDetailView(generics.RetrieveUpdateDestroyAPIView):
     Retrieve, update or delete a project instance
     """
     serializer_class = ProjectSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsActiveUser]
 
 
     def get_queryset(self):
@@ -59,7 +81,7 @@ class ContributorListView(generics.ListCreateAPIView):
     List all contributors or add a new contributor
     """
     serializer_class = ContributorSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsActiveUser]
 
 
     def get_queryset(self):
@@ -72,7 +94,7 @@ class ContributorDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Contributor.objects.all()
     serializer_class = ContributorSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsActiveUser]
 
 
 class IssueListCreateView(generics.ListCreateAPIView):
@@ -80,7 +102,7 @@ class IssueListCreateView(generics.ListCreateAPIView):
     List all issues or create a new issue
     """
     serializer_class = IssueSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsActiveUser]
 
 
     def get_queryset(self):
@@ -109,15 +131,30 @@ class IssueDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsActiveUser]
 
+
+    def get_queryset(self):
+        return Issue.objects.filter(project__contributors__user=self.request.user).select_related('author_user', 'assignee').prefetch_related('comments')
+
+
+    def perform_update(self, serializer):
+        if self.get_object().author_user != self.request.user:
+            raise PermissionDenied("Vous n'êtes pas autorisé à effectuer cette action")
+        serializer.save()
+
+
+    def perform_destroy(self, instance):
+        if instance.author_user != self.request.user:
+            raise PermissionDenied("Vous n'êtes pas autorisé à effectuer cette action")
+        instance.delete()
 
 class CommentListCreateView(generics.ListCreateAPIView):
     """
     List all comments or create a new comment
     """
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsActiveUser]
 
 
     def get_queryset(self):
@@ -125,11 +162,10 @@ class CommentListCreateView(generics.ListCreateAPIView):
         Return a list of comments that the user is a contributor to
         """
         return Comment.objects.filter(issue__project__contributors__user=self.request.user)
-    
+
 
     def perform_create(self, serializer):
         serializer.save(author_user=self.request.user)
-
 
 class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
@@ -137,4 +173,20 @@ class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsActiveUser]
+
+
+    def get_queryset(self):
+        return Comment.objects.filter(issue__project__contributors__user=self.request.user)
+
+
+    def perform_update(self, serializer):
+        if self.get_object().author_user != self.request.user:
+            raise PermissionDenied("Vous n'êtes pas autorisé à effectuer cette action")
+        serializer.save()
+
+
+    def perform_destroy(self, instance):
+        if instance.author_user != self.request.user:
+            raise PermissionDenied("Vous n'êtes pas autorisé à effectuer cette action")
+        instance.delete()
