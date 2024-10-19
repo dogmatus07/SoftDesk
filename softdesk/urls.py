87990342add22f53cebc5f524a504f6762bc05e6
Steve <steve.raharison@gmail.com>
2024-10-19 09:42:35 +0300

    Swagger/redoc oK

--- softdesk/urls.py
+++ softdesk/urls.py
@@ -44,9 +44,8 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # Swagger documentation
-    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # Swagger / Redoc documentation
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # Redoc documentation
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
